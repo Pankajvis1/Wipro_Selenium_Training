e0099d27db272a8ce065370abeacddb0ab1ee61c
pankajvishwakarma <pankajvishwakarma@localhost>
2026-04-21 11:54:10 +0530

    First Java code

---
+++ Testing_Wipro/src/Selenium/Test_Branch.java
@@ -0,0 +1,14 @@
+package Selenium;
+
+public class Test_Branch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing new Branch");
+		
+		
+
+	}
+
+}
